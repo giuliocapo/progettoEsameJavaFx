c607c18af85164f32f7afa6ed2320a3919582d7e
giuliocapo <caporrogiulio@gmail.com>
2022-02-17 11:40:39 +0100

    itWorks

--- src/main/java/com/example/progettoesamejavafx/Main.java
+++ src/main/java/com/example/progettoesamejavafx/Main.java
@@ -12,14 +12,27 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("menu.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 620, 620);
+        Scene scene = new Scene(fxmlLoader.load(), 620, 480);
         stage.setTitle("Menu");
         stage.setScene(scene);
         stage.show();
     }
     @FXML
     public void createLegendaryWeapon(){
-        WeaponFact.getInstance().createAWeapon("max","max","gold");
+       WeaponFact.getInstance().createAWeapon("max","max","gold");
+
+    }
+
+    @FXML
+    public void createRareWeapon(){
+        WeaponFact.getInstance().createAWeapon("medium","medium","purple");
+
+    }
+
+    @FXML
+    public void createNormalWeapon(){
+        WeaponFact.getInstance().createAWeapon("low","low","grey");
+        ;
     }
 
     public static void main(String[] args) {
