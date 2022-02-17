a72ddd2c53df99798eac3cd0f5ea31bc683c89f3
giuliocapo <caporrogiulio@gmail.com>
2022-02-17 11:00:33 +0100

    itWorks

--- src/main/java/com/example/progettoesamejavafx/main.java
+++ src/main/java/com/example/progettoesamejavafx/main.java
@@ -1,6 +1,7 @@
 package com.example.progettoesamejavafx;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -11,14 +12,15 @@ public class main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(main.class.getResource("menu.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 620, 620);
         stage.setTitle("Menu");
         stage.setScene(scene);
         stage.show();
     }
-
-    public void createAmmo1(){
-        WeaponFact.getInstance().createAWeapon("9","black","20");
+    @FXML
+    public void createLegendaryWeapon(){
+        WeaponFact.getInstance().createAWeapon("max","max","gold");
+        System.out.println("arma leggendaria creata!");
     }
 
     public static void main(String[] args) {
